22ca137ea252370a184a862c5d348e3ab4c12dec
Rafał Trzop <rafaltrzop@gmail.com>
2021-09-18 21:27:08 +0200

    Prettify code

--- webpack.common.js
+++ webpack.common.js
@@ -4,51 +4,41 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [
-        './src/scripts.js',
-        './src/styles.scss',
+  entry: ['./src/scripts.js', './src/styles.scss'],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Kalkulator wynagrodzenia B2B',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ],
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Kalkulator wynagrodzenia B2B',
-            template: './src/index.html',
-        }),
-    ],
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-        ],
-    },
-    optimization: {
-        minimizer: [
-            '...',
-            new CssMinimizerPlugin(),
-        ],
-        moduleIds: 'deterministic',
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
+  },
+  optimization: {
+    minimizer: ['...', new CssMinimizerPlugin()],
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
         },
+      },
     },
+  },
 };
