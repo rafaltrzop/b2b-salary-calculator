d0a599f79b1cf402b87d8acb96ed95112a96629c
Rafał Trzop <rafaltrzop@gmail.com>
2021-10-02 21:43:35 +0200

    Add checkbox options

--- src/scripts.js
+++ src/scripts.js
@@ -1,42 +1,66 @@
 import { add, divide, multiply } from 'mathjs';
 
+const sickDailyRateFactorInput = document.querySelector(
+  '#sick-daily-rate-factor'
+);
+const sickDaysCountInput = document.querySelector('#sick-days-count');
+const workDaysCountInput = document.querySelector('#work-days-count');
+
+function toggleSickLeaveFieldset() {
+  sickDailyRateFactorInput.toggleAttribute('required');
+  sickDaysCountInput.toggleAttribute('required');
+  document
+    .querySelector('#sick-leave-fieldset')
+    .classList.toggle('fieldset_hidden');
+}
+
+function togglePartTimeFieldset() {
+  workDaysCountInput.toggleAttribute('required');
+  document
+    .querySelector('#part-time-fieldset')
+    .classList.toggle('fieldset_hidden');
+}
+
 function calculateInvoiceValue(event) {
   event.preventDefault();
-  document.querySelector('#calc-output').classList.remove('fieldset_hidden');
+  document
+    .querySelector('#calc-output-fieldset')
+    .classList.remove('fieldset_hidden');
 
   const salary = document.querySelector('#salary').valueAsNumber;
-  const sickDailyRateFactor = divide(
-    document.querySelector('#sick-daily-rate-factor').valueAsNumber,
-    100
-  );
-  const sickDaysCount =
-    document.querySelector('#sick-days-count').valueAsNumber;
-  const workDaysCount =
-    document.querySelector('#work-days-count').valueAsNumber;
+
+  let sickDailyRateFactor = 1;
+  let sickDaysCount = 0;
+  if (sickLeaveCheckbox.checked) {
+    sickDailyRateFactor = divide(sickDailyRateFactorInput.valueAsNumber, 100);
+    sickDaysCount = sickDaysCountInput.valueAsNumber;
+  }
+
+  let workDaysCount = 21;
+  if (partTimeCheckbox.checked) {
+    workDaysCount = workDaysCountInput.valueAsNumber;
+  }
   const normalDaysCount = workDaysCount - sickDaysCount;
 
   const hourlyRate = divide(salary, 168);
-  const hourlyRateInput = document.querySelector('#hourly-rate');
-  hourlyRateInput.textContent = formatCurrency(hourlyRate);
-  hourlyRateInput.title = hourlyRate.toLocaleString();
+  const hourlyRateElement = document.querySelector('#hourly-rate');
+  hourlyRateElement.textContent = formatCurrency(hourlyRate);
+  hourlyRateElement.title = hourlyRate.toLocaleString();
 
   const dailyRate = divide(salary, 21);
-  const dailyRateInput = document.querySelector('#daily-rate');
-  dailyRateInput.textContent = formatCurrency(dailyRate);
-  dailyRateInput.title = dailyRate.toLocaleString();
+  const dailyRateElement = document.querySelector('#daily-rate');
+  dailyRateElement.textContent = formatCurrency(dailyRate);
+  dailyRateElement.title = dailyRate.toLocaleString();
 
   const sickDailyRate = multiply(dailyRate, sickDailyRateFactor);
-  const sickDailyRateInput = document.querySelector('#sick-daily-rate');
-  sickDailyRateInput.textContent = formatCurrency(sickDailyRate);
-  sickDailyRateInput.title = sickDailyRate.toLocaleString();
 
   const invoiceValue = add(
     multiply(normalDaysCount, dailyRate),
     multiply(sickDaysCount, sickDailyRate)
   );
-  const invoiceValueInput = document.querySelector('#invoice-value');
-  invoiceValueInput.textContent = formatCurrency(invoiceValue);
-  invoiceValueInput.title = invoiceValue;
+  const invoiceValueElement = document.querySelector('#invoice-value');
+  invoiceValueElement.textContent = formatCurrency(invoiceValue);
+  invoiceValueElement.title = invoiceValue;
 }
 
 function formatCurrency(number) {
@@ -46,6 +70,12 @@ function formatCurrency(number) {
     currencyDisplay: 'code',
   });
 }
+
+const sickLeaveCheckbox = document.querySelector('#sick-leave');
+sickLeaveCheckbox.addEventListener('change', toggleSickLeaveFieldset);
+
+const partTimeCheckbox = document.querySelector('#part-time');
+partTimeCheckbox.addEventListener('change', togglePartTimeFieldset);
 
 const invoiceValueForm = document.querySelector('#invoice-value-form');
 invoiceValueForm.addEventListener('submit', calculateInvoiceValue);
