ded344b1253538480499800aa266690c872a28ba
Rafał Trzop <rafaltrzop@gmail.com>
2021-10-16 20:40:30 +0200

    Add Jest testing framework

---
+++ jest.config.ts
@@ -0,0 +1,6 @@
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
+
+const config: InitialOptionsTsJest = {
+  preset: 'ts-jest',
+};
+export default config;
---
+++ src/scripts/salary-calculator.spec.ts
@@ -0,0 +1,5 @@
+describe('SalaryCalculator', () => {
+  it('should pass this test', () => {
+    expect(true).toBe(true);
+  });
+});
